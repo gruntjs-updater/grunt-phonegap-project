58f424dc51ea672d90df7325bc143f8f88e75004
svenlang <svenlang07@gmail.com>
2014-08-15 23:33:46 +0200

    clean report

--- tasks/phonegap_project.js
+++ tasks/phonegap_project.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
 
         var done = this.async(),
             UNDEFINED_ANDROID_MIN_SDK = -1,
-            isAndroidPlatformAdded = false,
+            isAndroidPlatformAdded = false, // todo check
 
             // require global options
             options = this.options({
@@ -40,20 +40,29 @@ module.exports = function(grunt) {
          * @param name {String} The key of the message to get
          * @returns {String} Returns the message
          * @example
-         *      getMessage("buildPlatform");
+         *      getMessage("buildPlatform", "iOS");
          */
         function getMessage(name) {
             name = _.isString(name) ? name : null;
-            // todo add arguments
+            var arguments_length = arguments.length,
+                i = 0,
+                output;
 
             var message = {
-                buildPlatform: 'Please wait, we build App Platform: ',
-                pathNoExists: 'The path no exists: ',
-                fileNoExists: 'The file no exists: ',
-                valueDeleteOptionsPathError: 'Check Variable "deleteOptionsPath".'
+                buildPlatform: 'Please wait, we build App Platform: {0}',
+                pathNoExists: 'The path no exists: {0}',
+                fileNoExists: 'The file no exists: {0}',
+                valueDeleteOptionsPathError: 'Check Variable "deleteOptionsPath"'
             };
+            output = message[name] || name;
 
-            return message[name] || name;
+            if (arguments_length > 1) {
+                for (i; i < arguments_length; i++) {
+                    output = output.replace(i - 1, arguments[i]);
+                }
+            }
+
+            return output;
         }
 
         /**
@@ -66,13 +75,13 @@ module.exports = function(grunt) {
             data = _.isObject(data) ? data : {};
             data.access = _.isArray(data.access) ? data.access : [];
 
-            // todo rename
             var file_www_config_xml = options.path + '/www/config.xml',
                 dataVersion = _.isString(options.version) && options.version.length > 0 ? options.version : null,
                 fileSource,
                 minSdkExp = /<preference\ name\=\"android\-minSdkVersion\"\ value\=\"[0-9]+\" \/\>/,
                 fileReplace = "";
 
+            // check if file exists
             if (grunt.file.isFile(file_www_config_xml)) {
 
                 // change version
@@ -90,13 +99,16 @@ module.exports = function(grunt) {
                 // change access
                 if (data.access.length > 0) {
 
-                    // delete default access
-                    fileSource = grunt.file.read(file_www_config_xml);
-                    grunt.file.write(file_www_config_xml, fileSource.replace(/<access\ origin\=\"\*\"\ \/\>/, ''));
-
                     data.access.forEach(function(url, index) {
 
                         if (_.isString(url)) {
+
+                            if (index === 1) {
+
+                                // delete default access
+                                fileSource = grunt.file.read(file_www_config_xml);
+                                grunt.file.write(file_www_config_xml, fileSource.replace(/<access\ origin\=\"\*\"\ \/\>/, ''));
+                            }
 
                             // create new access
                             fileSource = grunt.file.read(file_www_config_xml);
@@ -109,7 +121,7 @@ module.exports = function(grunt) {
                 }
 
             } else {
-                grunt.log.warn(getMessage('fileNoExists') + file_www_config_xml);
+                grunt.log.warn(getMessage('fileNoExists', file_www_config_xml));
                 done(false);
             }
         }
@@ -122,18 +134,18 @@ module.exports = function(grunt) {
          */
         function build(data) {
             data = _.isObject(data) ? data : {};
-            var items = _.isArray(data.platforms) ? data.platforms : [];
+            data.platforms = _.isArray(data.platforms) ? data.platforms : [];
 
             // check if app exists
             if (grunt.file.exists(options.path)) {
 
-                items.forEach(function(platform) {
+                data.platforms.forEach(function(platform) {
 
                     if (_.isString(platform)) {
 
                         // check is platforms installed
                         if (grunt.file.exists(options.path + '/platforms/' + platform)) {
-                            grunt.log.ok(getMessage('buildPlatform') + platform.toUpperCase());
+                            grunt.log.ok(getMessage('buildPlatform', platform.toUpperCase()));
 
                             // cordova build <platform>
                             grunt.util.spawn({
@@ -150,7 +162,7 @@ module.exports = function(grunt) {
                     }
                 });
             } else {
-                grunt.log.warn(getMessage('pathNoExists') + options.path);
+                grunt.log.warn(getMessage('pathNoExists', options.path));
             }
         }
 
@@ -216,7 +228,7 @@ module.exports = function(grunt) {
 
                     // check for android SDK
                     if (platform === 'android') {
-                        isAndroidPlatformAdded = true;
+                        isAndroidPlatformAdded = true; // todo check
                     }
 
                     // cordova platform add <platform>
@@ -241,6 +253,8 @@ module.exports = function(grunt) {
                             grunt.log.ok(result.stdout);
 
                             /*
+                             // todo delete
+                            // todo check
                             if (isAndroidPlatformAdded && grunt.file.isFile(options.path + '/' + fileAndroidManifest)) {
                                 replaceAndroidSdk();
                             }
